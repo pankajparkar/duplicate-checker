bb6a68fd77b3cea6c9cff26376057e31565c2513
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-07-13 18:12:04 +0530

    Rough logic added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// const isValidRange = ([firstNum, secondNum]) => {
+//   return firstNum && secondNum && firstNum < secondNum;
+// }
+
+const findDuplicates = (inputs, input) => {
+  return input.filter(i => inputs.has(i))
+}
+
 class App extends Component {
+  state = {
+    input: '',
+    messages: []
+  }
+  componentDidMount () {
+    const inputsMap = new Map();
+    [7000,7001,7002,7003,7004,7005].forEach(i => inputsMap.set(i, i));
+    this.setState({inputs: inputsMap})
+  }
+  onKeyDown = ({target: {value}}) => {
+    const {inputs} = this.state;
+    console.log(value)
+    // create number array
+    let nums = (value || '')
+      .split(',')
+      .map(n => n.trim())
+      .filter(n => n)
+      .map(Number);
+    // nums = [6098, 6009, 7000, 7001, 7002, 7003]
+    const duplicates = findDuplicates(inputs, nums)
+    // determine range and replace it with the numbers
+    // TODO: search multiple ranges
+    // const range = nums.find(n => n.indexOf('-') !== -1) || '';
+    // let rangeNumbers = [];
+    // if (range) {
+    //   rangeNumbers = range.split('-').map(Number);
+    // }
+    // // is Valid range
+    // if (isValidRange(rangeNumbers)) {
+    //   console.log(nums, range, rangeNumbers)
+    // }
+    // const 
+    console.log(nums, value, duplicates)
+    if(duplicates.length) {
+      const {messages} = this.state
+      messages.push(duplicates.join(', '))
+      this.setState({messages: messages})
+    }
+  }
   render() {
+    const {input, messages} = this.state;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>
+          <nav>
+            Range Matcher
+          </nav>
+        </header>
+        <section>
+          <article>
+            <textarea className="form-control" rows="6" onKeyDown={this.onKeyDown}></textarea>
+            {input}
+            <ul className="result">
+              {messages.map((m, index) => <li key={index}>{m}</li>)}
+            </ul>
+          </article>
+        </section>
       </div>
     );
   }
